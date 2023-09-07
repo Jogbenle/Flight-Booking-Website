3e36116999e2a11e48118585b2cc40303bb8b1e3
Gbenle John Olamide <gbenlejohn2rule@gmail.com>
2023-09-07 03:11:31 +0100

    Update Navbar.jsx

--- src/Components/navbar/Navbar.jsx
+++ src/Components/navbar/Navbar.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import {SiConsul} from 'react-icons/si'
 
 const Navbar = () => {
   return (
-    <div>
-        
+    <div className='navBar flex'>
+        <div className="navBarOne flex">
+            <div>
+                <SiConsul/>
+            </div>
+
+                <div className="flex">
+                    <li className='flex'></li>
+                </div>
+        </div>
     </div>
   )
 }
