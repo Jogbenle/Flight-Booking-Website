42a474fbc5dfa6a74cb5ccbe8d017d016a201d64
Gbenle John Olamide <gbenlejohn2rule@gmail.com>
2023-10-02 23:03:48 +0100

    Update Navbar.jsx

--- src/Components/navbar/Navbar.jsx
+++ src/Components/navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {SiConsul} from 'react-icons/si'
 import {BsPhoneVibrate} from 'react-icons/bs'
 import {AiOutlineGlobal} from 'react-icons/ai'
@@ -6,6 +6,11 @@ import {CgMenuGridO} from 'react-icons/cg'
 import logo from '../../assets/logo.png'
 
 const Navbar = () => {
+    const [active, setActive] = useState('navBarMenu')
+    const showNavBar = ()=>{
+        setActive('navBarMenu')
+    }
+
   return (
     <div className='navBar flex'>
         <div className="navBarOne flex">
@@ -29,7 +34,7 @@ const Navbar = () => {
                 <img src={logo} className='logo'/> 
             </div>
 
-            <div className="navBarMenu">
+            <div className={active}>
             <ul className="menu flex">
                 <li className="listItem">Home</li>
                 <li className="listItem">About</li>
@@ -43,12 +48,12 @@ const Navbar = () => {
             </button>
             </div>
 
-            <button className='btn flex btnOne'>
+            <button className='btn flex btnTwo'>
                 Contact
             </button> 
 
-            <div className="toggleIcon">
-                <CgMenuGridO/>
+            <div onClick={showNavBar} className="toggleIcon">
+                <CgMenuGridO className='icon'/>  
             </div>
 
         </div>
