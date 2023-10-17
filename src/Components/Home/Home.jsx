1cfc7124af26a101d18e51e539264a0892f85d3a
Gbenle John Olamide <gbenlejohn2rule@gmail.com>
2023-10-17 11:16:27 +0100

    Update Home.jsx

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -3,9 +3,12 @@ import React from 'react'
 const Home = () => {
   return (
     <div className='home flex container'>
-      
+
       <div className="mainText"> 
         <h1>Create A Long-lasting Memories with Us</h1>
+      </div>
+      <div className="homeVideo flex">
+        
       </div>
 
     </div>
